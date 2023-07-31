97b3d7937da8f02550886f2ea194434e6cf6e5f7
RntService <opuqo@rentgo.com>
2023-07-31 12:14:03 +0300

    Add project files.

---
+++ ObserverDesignPattern/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern
+{
+
+
+    // Gözlemci (Observer) arayüzü
+    public interface IObserver
+    {
+        void Update(string news);
+    }
+
+    // Somut Gözlemci (Concrete Observer) sınıfı
+    public class Subscriber : IObserver
+    {
+        private string name;
+
+        public Subscriber(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update(string news)
+        {
+            Console.WriteLine($"{name} received news: {news}");
+        }
+    }
+
+    // Konu (Subject) soyut sınıfı
+    public interface ISubject
+    {
+        void RegisterObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers(string news);
+    }
+
+    // Somut Konu (Concrete Subject) sınıfı
+    public class NewsSite : ISubject
+    {
+        private List<IObserver> observers;
+        private string latestNews;
+
+        public NewsSite()
+        {
+            observers = new List<IObserver>();
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers(string news)
+        {
+            latestNews = news;
+            foreach (var observer in observers)
+            {
+                observer.Update(latestNews);
+            }
+        }
+
+        public void AddNews(string news)
+        {
+            NotifyObservers(news);
+        }
+    }
+}
---
+++ ObserverDesignPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using ObserverDesignPattern;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Haber sitesi oluşturuyoruz
+        NewsSite newsSite = new NewsSite();
+
+        // İzleyicileri (gözlemcileri) oluşturuyoruz
+        IObserver subscriber1 = new Subscriber("John");
+        IObserver subscriber2 = new Subscriber("Alice");
+        IObserver subscriber3 = new Subscriber("Bob");
+
+        // İzleyicileri haber sitesine kaydediyoruz
+        newsSite.RegisterObserver(subscriber1);
+        newsSite.RegisterObserver(subscriber2);
+        newsSite.RegisterObserver(subscriber3);
+
+        // Haber ekliyoruz, izleyicilere bildirim gidecek
+        newsSite.AddNews("New product launched!");
+    }
+}
